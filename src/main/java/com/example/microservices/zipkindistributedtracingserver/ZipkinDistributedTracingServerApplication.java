7a27c2cc02d45c5e82ee091159325607b0bc11be
Sandhya <sandhyaj224@gmail.com>
2018-03-08 23:23:21 -0500

    Refractor

--- src/main/java/com/example/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
+++ src/main/java/com/example/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -1,9 +1,17 @@
 package com.example.microservices.zipkindistributedtracingserver;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import zipkin.server.EnableZipkinServer;
+
+/**
+ * 
+ * @author Sandhya
+ * Zipkin is used in server SpringBoot application
+ *
+ */
 
 @SpringBootApplication
 @EnableZipkinServer
